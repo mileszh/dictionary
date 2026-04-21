0b58f3b35ed12d96b822b0c40c25e0a7ec612594
mileszh <mileszh05@gmail.com>
2026-04-20 20:40:35 -0400

    dictionary-05: Refactor extractFromEntry with TypeScript generics

--- src/main.ts
+++ src/main.ts
@@ -37,9 +37,12 @@ const searchWord = async (word: string): Promise<DictionaryAPIResponse[]> => {
   return data;
 };
 
-const extractFromEntry = (data, key) => {
+const extractFromEntry = <K extends keyof DictionaryAPIResponse>(
+  data: DictionaryAPIResponse[] | undefined,
+  key: K,
+): DictionaryAPIResponse[K] | undefined => {
   if (data && Array.isArray(data)) {
-    return data[0][key];
+    return data?.[0]?.[key] ?? undefined;
   }
 };
 
